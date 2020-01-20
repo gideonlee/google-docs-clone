1b090635227e97e6fd7c0669d7ef4fe2503204f2
Gideon Lee <gideonxlee@gmail.com>
2020-01-20 14:11:35 -0800

    Removed unnecessary commented lines

--- src/app/services/document.service.ts
+++ src/app/services/document.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-// import { Socket } from 'ngx-socket-io';
 import { Document } from '../models/document';
 
 @Injectable({
